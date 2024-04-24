6154e371c4f6a8fc403d35912233cc460f13e6b4
MuroyaIan <87190478+MuroyaIan@users.noreply.github.com>
2024-04-24 20:58:20 +0900

    ファイル操作クラス追加

---
+++ 0424_FileMgr/FileMgr.cpp
@@ -0,0 +1,64 @@
+﻿//===== インクルード部 =====
+#include  "FileMgr.h"
+#include <fstream>
+
+//===== クラス実装 =====
+FileObject::FileObject(const std::string& fileName) noexcept
+{
+	//ファイル読込
+	openFile(fileName);
+}
+
+/**
+ * ファイル読込
+ *
+ * \param fileName
+ * \return void
+ */
+void FileObject::openFile(const std::string& fileName) noexcept
+{
+	//ファイル名登録
+	m_strName = fileName;
+	m_strFile.clear();
+
+	//ファイルの中身を取得
+	std::ifstream file(m_strName);
+	char c{'\0'};
+	while (file.get(c))
+		m_strFile += c;
+	file.close();
+
+	//イテレータ初期化
+	m_itr = m_strFile.begin();
+}
+
+/**
+ * 文字取得
+ *
+ * \param c
+ * \return bool
+ */
+bool FileObject::get(char& c) noexcept
+{
+	//末尾まで１文字ずつ取り出す
+	if (m_itr != m_strFile.end())
+	{
+		c = *m_itr;
+		++m_itr;
+		return true;
+	}
+
+	//取り出す終了
+	return false;
+}
+
+/**
+ * イテレータをリセット
+ *
+ * \param
+ * \return void
+ */
+void FileObject::resetItr() noexcept
+{
+	m_itr = m_strFile.begin();
+}
---
+++ 0424_FileMgr/FileMgr.h
@@ -0,0 +1,39 @@
+﻿/**
+ * @file FileMgr.h
+ * @brief ファイル操作クラス
+ * @author Muroya Ian
+ * @date 2024/04/25
+ */
+
+//===== インクルードガード =====
+#pragma once
+
+//===== インクルード部 =====
+#include <string>
+
+//===== クラス定義 =====
+class FileObject
+{
+public:
+
+	//コピー＆ムーブ
+	FileObject(const FileObject&) = delete;
+	FileObject& operator =(const FileObject&) = delete;
+	FileObject(FileObject&&) noexcept = delete;
+	FileObject& operator=(FileObject&&) noexcept = delete;
+
+	//プロトタイプ宣言
+	explicit FileObject() noexcept = default;
+	explicit FileObject(const std::string& fileName) noexcept;
+	~FileObject() noexcept = default;
+	void openFile(const std::string& fileName) noexcept; //ファイル読込
+	bool get(char& c) noexcept;                          //文字取得
+	void resetItr() noexcept;                            //イテレータをリセット
+
+private:
+
+	//変数宣言
+	std::string m_strName;       //ファイル名
+	std::string m_strFile;       //ファイルの中身
+	std::string::iterator m_itr; //文字取得用
+};
